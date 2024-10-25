9752939f77de4a2a2ff0c14fc3ccd2147dbbfe97
alexanderthoren <alexitoblogger@gmail.com>
2024-10-25 23:22:08 +0200

    feat(tvOS): add tvOS support to the project
    - Added `MyApp.swift` for tvOS platform.
    - Updated `project.pbxproj` to include tvOS target and configurations.
    - Renamed iOS scheme to `multiplatform_modularized_project iOS.xcscheme`.
    - Added new scheme for `multiplatform_modularized_project watchOS Watch App`.

---
+++ multiplatform_modularized_project tvOS/MyApp.swift
@@ -0,0 +1,18 @@
+//
+//  MyApp.swift
+//  multiplatform_modularized_project tvOS
+//
+//  Created by Alexander Thoren on 25/10/24.
+//
+
+import Application
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
