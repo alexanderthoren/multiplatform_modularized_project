158b891164704ef37d3f7525c4a2a05939c42a37
alexanderthoren <alexitoblogger@gmail.com>
2024-10-25 19:43:15 +0200

    Added base configs.

---
+++ multiplatform_modularized_project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  multiplatform_modularized_project
+//
+//  Created by Alexander Thoren on 25/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ multiplatform_modularized_project/MyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyApp.swift
+//  multiplatform_modularized_project
+//
+//  Created by Alexander Thoren on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
